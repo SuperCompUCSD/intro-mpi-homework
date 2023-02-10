2d10c109e5836cc620a361629031bcd5bae11013
Khai Vu <khaitvu@gmail.com>
2023-02-10 11:25:37 -0800

    Add files via upload
    initial

---
+++ Makefile
@@ -0,0 +1,18 @@
+EXE = main
+OBJS = main.o
+CC = gcc
+DEBUG = -g
+WARN = -Wall -Wextra
+CFLAGS = -std=gnu11 $(DEBUG) $(WARN)
+LIBS = -lm
+.PHONY: clean run
+
+$(EXE) : $(OBJS)
+	$(CC) $(CFLAGS) -o $@ $(OBJS) $(LIBS)
+
+run:
+	make
+	./$(EXE)
+
+clean:
+	rm -f $(EXE) $(OBJS)
---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+	const unsigned int numdims = 2;
+	double dims[numdims];
+	double c_circle=0, c_cartioid=0;
+	
+	for (int i = 0; i < 2000; i++){
+		for (unsigned int j = 0; j < numdims; j++){
+			dims[j] = 2*(double) rand() / (double) (RAND_MAX) -1;
+		}
+		//printf("%f\n", x);
+		c_circle += (pow(dims[0],2) + pow(dims[1],2) < 1)? 1 : 0;
+		c_cartioid += (pow(pow(dims[0],2) + pow(dims[1],2), 2) + dims[0] * (pow(dims[0],2) + pow(dims[1], 2)) - 0.25*pow(dims[1], 2) < 0) ? 1: 0;
+		//printf("%lf\t %lf\n", x,y);
+		printf("%.30lf\n", c_cartioid/c_circle);
+	}
+
+	return 0;
+}
