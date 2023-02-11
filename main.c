526cc85c5f88a75846725a714674d4c7ff0cd1aa
Khai Vu <khaitvu@gmail.com>
2023-02-10 16:12:27 -0800

    comments

--- main.c
+++ main.c
@@ -8,10 +8,12 @@ int main(){
 	double c_circle=0, c_cartioid=0;
 	
 	for (int i = 0; i < 2000; i++){
+		// gen rand coord
 		for (unsigned int j = 0; j < numdims; j++){
 			dims[j] = 2*(double) rand() / (double) (RAND_MAX) -1;
 		}
 		//printf("%f\n", x);
+		//check if coord is in what is within shape
 		c_circle += (pow(dims[0],2) + pow(dims[1],2) < 1)? 1 : 0;
 		c_cartioid += (pow(pow(dims[0],2) + pow(dims[1],2), 2) + dims[0] * (pow(dims[0],2) + pow(dims[1], 2)) - 0.25*pow(dims[1], 2) < 0) ? 1: 0;
 		//printf("%lf\t %lf\n", x,y);
